c52529b0b1e89568b0134eb8bcaeb591bb737642
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-11-13 17:35:11 +0100

    allow waiting on server instance

--- src/Preface/Server.hs
+++ src/Preface/Server.hs
@@ -56,6 +56,17 @@ startAppServer serverAssignedName allowedOrigins listenPort makeApp = do
             destination = Callback (\str -> logInfo logger (decode @Value $ fromStrict $ fromLogStr str))
           }
 
+-- | Wait for the server to terminate its execution.
+--
+--  * If the server's inner thread throws an execution, it will be rethrown.
+--  * If the server is not started, it returns immediately.
+--
+-- This a simple wrapper over `Control.Concurrent.Async.wait` in order to alleviate
+-- the need to have an explicitly dependency on it.
+waitServer :: AppServer -> IO ()
+waitServer (AppServer (Just thread) _ _) = wait thread
+waitServer _ = pure ()
+
 stopServer :: AppServer -> IO ()
 stopServer (AppServer (Just thread) _ _) = cancel thread
 stopServer _ = pure ()
